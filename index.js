146abe533dc846b794202fd01c908842a56180a3
rnrabi <rnrabi913@gmail.com>
2023-05-28 22:46:10 +0530

    server ok

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
 
 
@@ -12,18 +14,60 @@ app.use(express.json())
 
 
 
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.rjjtc94.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const menuCollection = client.db('bistrobossDB').collection('menu');
+        const reviewsCollection = client.db('bistrobossDB').collection('reviews')
+
+        app.get('/menu' , async(req ,res)=>{
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/reviews' , async(req , res)=>{
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
 
 
 
-app.get('/' , (req , res) =>{
+
+
+app.get('/', (req, res) => {
     res.send('bistro boss is sitting')
 })
 
-app.listen(port , ()=>{
-    console.log('bistro boss port is:' , port)
+app.listen(port, () => {
+    console.log('bistro boss port is:', port)
 })
 
 
